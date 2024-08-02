2c69afab83d6100354787ab910965e21277ae568
nasimqureshi <qureshi.nasim@yahoo.com>
2024-08-02 16:05:27 -0500

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const Header = () => {
+  return (
+    <header className="header">
+    <h1>Expenses Tracker</h1>
+       <p>by nasim</p>
+    </header>
+  )
+}
